96ef9402dceed14e55d248541b84cc9f6d0ab0dd
MorganJay <jetmorgan.jm@gmail.com>
2020-04-02 08:17:48 +0100

    02/04/2020

---
+++ First Webpage/script.js
@@ -0,0 +1,15 @@
+// let name = "Jeff";
+
+//  console.log("My name is: " + name);
+
+//  const data = [];
+
+//  for (let i=0; i<=100; i++) {
+
+//    if (i%2===0) {
+
+//      data.push(i);
+//    }
+//  }
+// alert(data)
+//  console.log(data);
