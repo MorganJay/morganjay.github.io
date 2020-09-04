81e17d3346682f7e895e160224ebbfccb2000fe2
James Morgan <jetmorgan.jm@gmail.com>
2020-09-04 23:21:57 +0100

    jsss

--- Complete Web Dev/JavaScript/DOM/script.js
+++ Complete Web Dev/JavaScript/DOM/script.js
@@ -35,3 +35,45 @@
 // children
 
 // ##It is important to CACHE selectors in variables
+//  var h1 = document.querySelector("h1");
+
+//  var button = document.getElementsByTagName("button")[0];
+
+//  button.addEventListener("click", function(){
+//      console.log("CLICKK");
+//  });
+
+//  button.addEventListener("mouseleave", function(){
+//      console.log("mouse has been left");
+//  })
+
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function inputLength() {
+  return input.value.length;
+}
+
+function createListElement() {
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
+}
+
+function addListAfterCLick() {
+  if (inputLength() > 0) {
+    createListElement();
+  }
+}
+
+function addListAfterKeypress(event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
+}
+
+button.addEventListener("click", addListAfterCLick());
+
+input.addEventListener("keypress", addListAfterKeypress());
