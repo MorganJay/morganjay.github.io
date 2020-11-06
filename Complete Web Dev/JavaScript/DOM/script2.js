dbcdc681b1d561c53f818ca40b627600f0491f4a
James Morgan O <MorganJay@users.noreply.github.com>
2020-11-06 11:20:44 +0100

    finally o

--- Complete Web Dev/JavaScript/DOM/script2.js
+++ Complete Web Dev/JavaScript/DOM/script2.js
@@ -1,37 +1,137 @@
 //Cacheing selectors
-var addbutton = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ol = document.querySelector("ol");
-var delButton = document.querySelectorAll(".delete");
-var li = document.querySelectorAll("li");
+const addButton = document.getElementById("enter");
+const input = document.getElementById("userinput");
+const ol = document.querySelector("ol");
+const tasksNo = document.querySelector(".tasks");
+const searchInput = document.querySelector(".search")
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+const date = document.querySelector(".date");
+const doneTasks = JSON.parse(localStorage.getItem("donetodos"))||[];
+// import Swal from 'sweetalert2';
+// const Swal = require('sweetalert2');
+
+if (todos) {
+  todos.forEach((todo) => {
+    if(doneTasks.includes(todo)){
+      ol.innerHTML += `
+      <div class="list-container">
+          <li class="done">${todo}</li>
+          <button type="button" class="delete">×</button>
+      </div>`;
+    }
+    else{
+      ol.innerHTML += `
+      <div class="list-container">
+          <li>${todo}</li>
+          <button type="button" class="delete">×</button>
+      </div>`;
+    }           
+  });
+  numberTasks();
+  const delButton = document.querySelectorAll(".delete");
+  const li = document.querySelectorAll("li");
+  li.forEach((list_item) => {
+    list_item.addEventListener("click", crossItem);
+  });
+  delButton.forEach((delBtn) => {
+    delBtn.addEventListener("click", delItem);
+  });
+}
+
+function getDate() {
+  // Get dates
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const d = new Date();
+  const dayName = days[d.getDay()];
+  const monthName = monthNames[d.getMonth()];
+  const day = (d.getDay() + 1);
+  const year = d.getFullYear();
+  date.textContent = `${dayName} ${day} ${monthName}, ${year}`;
+}
 
 function inputLength() {
   return input.value.length;
 }
 
+function numberTasks() {
+  if (todos.length === 1) {
+    tasksNo.textContent = `${todos.length} task`;
+  } else if (todos.length >= 2) {
+    tasksNo.textContent = `${todos.length} tasks`;
+  } else {
+    tasksNo.textContent = "";
+  }
+  return todos.length;
+}
+
+function storeTodos() {
+  todos.unshift(input.value);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
 function createListElement() {
-  let li = document.createElement("li");
-  li.innerHTML = ` <p class="title">${input.value}</p> 
-         <div class="button-container">
-             <button type="button" class="delete">×</button>
-         </div>`;
-  ol.appendChild(li);
+  storeTodos();
+  let div = document.createElement("div");
+  div.classList.add("list-container");
+  div.innerHTML = `<li>${input.value}</li>
+  <button type="button" class="delete">×</button>`;
+  ol.prepend(div);
+  numberTasks();
   input.value = "";
-  const list_item = li.querySelector(".title");
-  list_item.addEventListener("click", crossItem);
-  const delBtn = li.querySelector(".delete");
+  const li = div.firstElementChild;
+  li.addEventListener("click", crossItem);
+  const delBtn = div.querySelector(".delete");
   delBtn.addEventListener("click", delItem);
 }
 
-function crossItem(e)
-{
-    const list_item = e.currentTarget.parentElement;
-    list_item.classList.toggle("done");
+function crossItem(e) {
+  const list_item = e.currentTarget;
+  let listText = list_item.textContent;
+  if (list_item.classList.contains("done")) {
+    list_item.classList.remove("done");
+    let index = doneTasks.indexOf(listText);
+    doneTasks.splice(index, 1);
+    localStorage.setItem("donetodos", JSON.stringify(doneTasks));
+  } else {
+    list_item.classList.add("done");
+    doneTasks.push(listText);
+    localStorage.setItem("donetodos", JSON.stringify(doneTasks));
+    Swal.fire('Good job!','Task completed','success');
+  }
 }
 
 function delItem(e) {
-    const listitem = e.currentTarget.parentElement. parentElement;
-    ol.removeChild(listitem);
+  const listItem = e.path[1];
+  const listItemContent = listItem.children[0].innerHTML;
+  const index = todos.indexOf(listItemContent);
+  ol.removeChild(listItem);
+  todos.splice(index, 1);
+  numberTasks();
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 function addListAfterCLick() {
@@ -48,11 +148,18 @@ function addListAfterKeypress(event) {
   }
 }
 
-addbutton.addEventListener("click", addListAfterCLick);
+function searchTodos(e){
+  const input = e.target.value.toLowerCase();
+  Array.from(todos).forEach((todo) => {
+      if (todo.toLowerCase().indexOf(input) != -1) {
+        todo.style.display = "block";
+      } else {
+        todo.style.display = "none";
+      }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", getDate);
+addButton.addEventListener("click", addListAfterCLick);
 input.addEventListener("keypress", addListAfterKeypress);
-li.forEach((list_item) => {
-    list_item.addEventListener("click", crossItem);
-});
-delButton.forEach((delBtn) => {
-  delBtn.addEventListener("click", delItem);
-});
+searchInput.addEventListener('keyup', searchTodos);
